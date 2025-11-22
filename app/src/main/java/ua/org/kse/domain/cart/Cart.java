b1f1cc3bf39f54cabbc1a7fabd5c8b945069060c
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2025-11-22 19:20:41 +0200

    Resolve the issues related to the Cart

--- app/src/main/java/ua/org/kse/domain/cart/Cart.java
+++ app/src/main/java/ua/org/kse/domain/cart/Cart.java
@@ -1,11 +1,13 @@
 package ua.org.kse.domain.cart;
 
+import lombok.Builder;
 import lombok.Data;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Data
+@Builder
 public class Cart {
     private String id;
-    private List<String> productIds = new ArrayList<>();
+    private List<String> productIds;
 }
