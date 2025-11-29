6c6b55166d9302c78a907c3802aae364721348f6
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2025-11-29 23:07:26 +0200

    Fix integration tests

--- app/src/test/java/ua/org/kse/support/AbstractPostgresIT.java
+++ app/src/test/java/ua/org/kse/support/AbstractPostgresIT.java
@@ -1,10 +1,12 @@
 package ua.org.kse.support;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Testcontainers
 public abstract class AbstractPostgresIT {
     @Container
